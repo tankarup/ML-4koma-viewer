4bb37d7cc36fded29174123a55af8101f009fd3b
tankarup <69491808+tankarup@users.noreply.github.com>
2021-06-06 05:47:54 +0900

    メニューを標準的な並びに

--- main.js
+++ main.js
@@ -87,9 +87,19 @@ function get_person_list(key){
     return person_list;
 }
 
+function get_idol_list(){
+	let idol_names_from_data = get_person_list('idols');
+	let idol_list = idol_name_standard_list;
+
+	for (let name of idol_names_from_data){
+		if (idol_list.indexOf(name) == -1) idol_list.push(name);
+	}
+	return idol_list;
+}
+
 function init_menu(){
 
-    idol_list = get_person_list('idols');
+    idol_list = get_idol_list();
     drawer_list = get_person_list('drawers');
 
     //アイドルフィルターメニュー作成
@@ -244,3 +254,89 @@ function update_tweets(stories){
     
 }
 
+/*
+カプ検索と同じ内容
+*/
+let idol_name_standard_list = [];// 標準的なアイドル名の並び　[春香, 千早, ...]
+let idol_name_dic = {}; //idol_name_dic[天海春香] → 春香
+/*
+normalize_name(天海春香)→ 春香
+*/
+function normalize_name(name){
+    const nomalized_name = idol_name_dic[name];
+    return nomalized_name ? nomalized_name : name;
+}
+
+const idol_names = `春香	天海春香		
+千早	如月千早		
+美希	星井美希		
+雪歩	萩原雪歩		
+やよい	高槻やよい		
+真	菊地真		
+伊織	水瀬伊織		
+貴音	四条貴音		
+律子	秋月律子		
+あずさ	三浦あずさ		
+亜美	双海亜美		
+真美	双海真美		
+響	我那覇響		
+未来	春日未来		
+静香	最上静香		
+翼	伊吹翼		
+琴葉	田中琴葉		
+エレナ	島原エレナ		
+美奈子	佐竹美奈子		
+恵美	所恵美		
+まつり	徳川まつり		
+星梨花	箱崎星梨花		
+茜	野々原茜		
+杏奈	望月杏奈		
+ロコ	ロコ	路子	伴田路子
+百合子	七尾百合子		
+紗代子	高山紗代子		
+亜利沙	松田亜利沙		
+海美	高坂海美		
+育	中谷育		
+朋花	天空橋朋花		
+エミリー	エミリー	エミリースチュアート	エミリー スチュアート
+志保	北沢志保		
+歩	舞浜歩		
+ひなた	木下ひなた		
+可奈	矢吹可奈		
+奈緒	横山奈緒		
+千鶴	二階堂千鶴		
+このみ	馬場このみ		
+環	大神環		
+風花	豊川風花		
+美也	宮尾美也		
+のり子	福田のり子		
+瑞希	真壁瑞希		
+可憐	篠宮可憐		
+莉緒	百瀬莉緒		
+昴	永吉昴		
+麗花	北上麗花		
+桃子	周防桃子		
+ジュリア	ジュリア		
+紬	白石紬		
+歌織	桜守歌織		
+小鳥	音無小鳥		
+美咲	青羽美咲		
+社長	高木順二朗	高木社長	
+劇子	劇場の魂		
+黒井社長	黒井崇男		
+`;
+
+let idol_lines = idol_names.split('\n');
+for (let idol_line of idol_lines){
+	if (idol_line.length < 1) continue;
+    let names = idol_line.split('\t');
+    names = names.map(function(name){
+        return name.trim();
+    });
+	idol_name_standard_list.push(names[0]);
+    for (let i = 0; i < names.length; i++){
+        if (idol_name_dic[names[i]]) continue;
+        idol_name_dic[names[i]] = names[0];
+    }
+
+}
